294976586633270efd3d8c8d2599c40a5c444f53
PhaseRush <leozhang888@yahoo.ca>
2018-11-12 15:07:11 -0800

    Added custom urls for default genres

--- src/main/java/main/utility/music/MusicUtils.java
+++ src/main/java/main/utility/music/MusicUtils.java
@@ -30,5 +30,12 @@ public class MusicUtils {
         customUrls.put("nb3all", "https://www.youtube.com/watch?v=BwEZaariQQ4&list=PLEgNqLmZpLuI9ajUy3Hg97NrpssG4repu");
         customUrls.put("nb3", "https://www.youtube.com/watch?v=yLxsJpgvkfo&list=PLwMEL7UNT4o9iMzrvNBXZqXbNPFfT6rVD");
         customUrls.put("dank", "https://www.youtube.com/playlist?list=PLSbBQFh_CUqufnCqVuAfjw7pwJzuu2UNe");
+
+        //youtube defaults
+        customUrls.put("pop", "https://www.youtube.com/playlist?list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj");
+        customUrls.put("trap", "https://www.youtube.com/playlist?list=PLPgtsFyiwUoLAFd6-yNNtrF3l9H1EQ9Nx");
+        customUrls.put("fbass", "https://www.youtube.com/playlist?list=PLe8jmEHFkvsbRwwi0ode5c9iMQ2dyJU3N");
+        customUrls.put("lofi", "https://www.youtube.com/watch?v=hHW1oY26kxQ");
+
     }
 }
