74e8e154fc39d30f3dfc30b4272a075d0a529511
PhaseRush <leozhang888@yahoo.ca>
2019-03-16 01:08:54 -0700

    Voice joining - simplified comparator and cleamed up indenting

--- src/main/java/main/commands/music/playing/JoinVoiceChannel.java
+++ src/main/java/main/commands/music/playing/JoinVoiceChannel.java
@@ -6,6 +6,7 @@ import main.utility.metautil.BotUtils;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class JoinVoiceChannel implements Command {
@@ -17,21 +18,12 @@ public class JoinVoiceChannel implements Command {
             event.getGuild().getVoiceChannels().stream()
                     .map(IChannel::getName)
                     .map(name -> new Pair<>(name, name.toLowerCase()))
-                    .sorted((o1, o2) -> {
-                        double v1 = Math.min(
-                                BotUtils.stringSimilarity(o1.getKey(), args.get(0)),
-                                BotUtils.stringSimilarity(o1.getValue(), args.get(0).toLowerCase())
-                        );
-                        double v2 = Math.min(
-                                BotUtils.stringSimilarity(o2.getKey(), args.get(0)),
-                                BotUtils.stringSimilarity(o2.getValue(), args.get(0).toLowerCase())
-                        );
-                        if (v1 == v2) return 0;
-                        return v1 < v2 ? -1 : 1;
-                    })
+                    .sorted(Comparator.comparingDouble(o -> Math.min(
+                            BotUtils.stringSimilarity(o.getKey(), args.get(0)),
+                            BotUtils.stringSimilarity(o.getValue(), args.get(0).toLowerCase()))))
                     .filter(pair ->
                             BotUtils.stringSimilarity(pair.getKey(), args.get(0)) < Math.max(2, pair.getKey().length()/5) ||
-                                    BotUtils.stringSimilarity(pair.getValue(), args.get(0).toLowerCase()) < Math.max(2, pair.getValue().length()/5))
+                            BotUtils.stringSimilarity(pair.getValue(), args.get(0).toLowerCase()) < Math.max(2, pair.getValue().length()/5))
                     .findFirst()
                     .ifPresent(pair -> event.getGuild().getVoiceChannelsByName(pair.getKey()).get(0).join());
         }
