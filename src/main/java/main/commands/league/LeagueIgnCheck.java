1474a508f76aaa28548aa0caa74cf97f3e2277f6
PhaseRush <leozhang888@yahoo.ca>
2019-04-02 20:05:54 -0700

    League name checking more robust with better help and spaces

--- src/main/java/main/commands/league/LeagueIgnCheck.java
+++ src/main/java/main/commands/league/LeagueIgnCheck.java
@@ -13,7 +13,9 @@ import java.util.Optional;
 public class LeagueIgnCheck implements Command {
     @Override
     public void runCommand(MessageReceivedEvent event, List<String> args) {
-        String html = BotUtils.getStringFromUrl("https://lolnames.gg/en/na/" + BotUtils.concatArgs(args) + "/");
+        String url = "https://lolnames.gg/en/na/" + args.get(0).replaceAll("[_\\s]", "%20") + "/";
+        System.out.println(url);
+        String html = BotUtils.getStringFromUrl(url);
         Optional<Element> element = Jsoup.parse(html).getAllElements().stream()
                 .filter(e -> e.hasClass("text-center"))
                 .filter(e -> e.tag().getName().equals("h4"))
@@ -22,11 +24,16 @@ public class LeagueIgnCheck implements Command {
         element.ifPresent(element1 -> BotUtils.send(event.getChannel(),
                 new EmbedBuilder()
                         .withTitle(element1.text())
-                        .withDesc("[Add to Google Calendar](" + getCalUrl(html)+ ")")));
-
+                        .withDesc(genDesc(html, url))));
     }
 
-    private String getCalUrl(String html) {
+    private String genDesc(String html, String url) {
+        return "[See for yourself](" + url + ")"
+                + (genCal(html) == null ? "" :
+                "\n[Add to Google Calendar](" + genCal(html)+ ")");
+    }
+
+    private String genCal(String html) {
         try {
             int startIdx = html.indexOf("http://www.google.com/calendar/render?action=TEMPLATE&text=Summoner+name+");
             html = html.substring(startIdx);
@@ -34,7 +41,7 @@ public class LeagueIgnCheck implements Command {
             return html.substring(0, endIdx);
         } catch (Exception e) {
             e.printStackTrace();
-            return "not available";
+            return null;
         }
     }
 
@@ -45,6 +52,8 @@ public class LeagueIgnCheck implements Command {
 
     @Override
     public String getDesc() {
-        return "Gives time until username is available";
+        return "Gives time until username is available. \n" +
+                "NOTE: if you want to check a name with spaces, replace STARTING and/or ENDING spaces with underscore \"_\"\n" +
+                "Example: \" Kat Attack\" :arrow_right: \"_Kat Attack\"";
     }
 }
